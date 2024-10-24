9cc04d97e3466473c95c876bf043dc30ee5853c9
hadi-saleh14 <hadi@creatoros.co>
2024-10-24 17:46:56 +0300

    Update identity_digest.rs

--- pallets/pallet-reclaim/src/identity_digest.rs
+++ pallets/pallet-reclaim/src/identity_digest.rs
@@ -3,16 +3,17 @@ use digest::{
 	Reset, Update,
 };
 
-/// The 256-bits identity container
+/// A container for a 256-bit identity, represented as a fixed-size array.
 #[derive(Clone, Default)]
 pub struct Identity256 {
 	array: GenericArray<u8, U32>,
 }
 
 impl Update for Identity256 {
+    	/// Updates the identity with a new 32-byte hash.
 	fn update(&mut self, hash: &[u8]) {
-		assert_eq!(hash.as_ref().len(), 32);
-		self.array = *GenericArray::from_slice(hash);
+		assert_eq!(hash.as_ref().len(), 32); // Ensure the input length is 32 bytes.
+		self.array = *GenericArray::from_slice(hash);	// Update the array with the new hash.
 	}
 }
 
@@ -21,14 +22,16 @@ impl OutputSizeUser for Identity256 {
 }
 
 impl FixedOutput for Identity256 {
+    /// Finalizes the hashing process and outputs the result.
 	fn finalize_into(self, out: &mut Output<Self>) {
 		*out = self.array;
 	}
 }
 
-impl HashMarker for Identity256 {}
+impl HashMarker for Identity256 {}	// Marker trait for identifying as a hash function.
 
 impl Reset for Identity256 {
+    /// Resets the identity back to its default state.
 	fn reset(&mut self) {
 		*self = Self::default();
 	}
